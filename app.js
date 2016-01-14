35452d4eef02767a18245d83c4ab15ff4f311a38
nmsbane <madhusudhan638@gmail.com>
2016-01-14 12:46:59 +0530

    configured express framework for routes, socket.io library, and use the static framework of express

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express'), 
+	path  = require('path');
+	
+	
+var app = express();
+
+app.set('views', path.join(__dirname, 'views')); // name and path to the folder which stores the views, i.e .html files
+app.engine('html', require('hogan-express')); // express should render html files and the method to use to render pages is hogan-express module
+app.set('view engine', 'html'); // file extension for templates is html
+
+app.use(express.static(path.join(__dirname, 'public'))); // for static files look in the public folder
+
+// set the port
+app.set('port', process.env.PORT || 3000);
+
+// use the router middleware
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+	res.render('index', {});
+});
+
+// mount the router on the app, i.e, whenever the request comes, use router.
+app.use('/', router);
+
+// configure socket.io for express framework
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+//use server instead of app, as server wraps app around it. So use .listen method on the 'server' variable
+//since we used app.set to set the port, we use app.get('port')
+server.listen(app.get('port'), function(){
+	console.log("PHOTOGRID running on port " + app.get('port'));
+});
