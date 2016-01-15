0fe181801c984de0cd88941f56a969599d98272e
nmsbane <madhusudhan638@gmail.com>
2016-01-15 12:38:18 +0530

    now the frontend can send ajax calls to the /upload url, and about to write code to handle the uploaded file

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,13 @@ module.exports = function(express, app){
 	var router = express.Router();
 	
 	router.get('/', function(req, res, next){
-		res.render('index', {});
+		res.render('index', {'host': app.get('host')});
 	});
+	
+	// to handle /upload view point
+	router.post('/upload', function(req, res, next) {
+		// manage file upload
+	})
 	
 	// mount the router on the app, i.e, whenever the request comes, use router.
 	app.use('/', router);
